f68607759fbd5c1ecb6c226e559860a77ef8d757
gbhoot <gsbhoot1020@gnail.com>
2019-04-27 19:48:13 -0700

    fixed axios import

--- moviez-app/src/main.js
+++ moviez-app/src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-import axois from 'axios'
+import axios from 'axios'
 
 Vue.config.productionTip = false
-axois.defaults.baseURL = 'http://www.omdbapi.com/?apikey=b76b385c&page=1&type=movie&Content-Type=application/json'
+axios.defaults.baseURL = 'http://www.omdbapi.com/?apikey=b76b385c&page=1&type=movie&Content-Type=application/json'
 
 new Vue({
     render: h => h(App),
